1b471aa859bbee33be4add7d071121ce1b8a6a74
Alex Ewerlöf <alex.ewerlof@schibsted.com>
2018-03-08 23:13:28 +0100

    the wrong way of toggling (mutating props)

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 export default function TodoItem(props) {
-    return <li key={props.id}>
-        <label>
-            <input type="checkbox" checked={props.done} />
-            {props.title}
-        </label>
-    </li>
+
+    return <label>
+        <input
+            type="checkbox"
+            checked={props.done}
+            onChange={event => props.toggleDone(props.id)} />
+        {props.title}
+    </label>
 }
--- src/TodoList.js
+++ src/TodoList.js
@@ -2,12 +2,28 @@ import React from 'react';
 import TodoItem from './TodoItem';
 
 export default class TodoList extends React.Component {
+
+
+    toggleDone = (id) => {
+        const item = this.props.items.find(i => i.id === id);
+        if (item) {
+            item.done = !item.done;
+            console.log(item)
+        } else {
+            console.log('no item with id', id)
+        }
+    }
+
     render() {
+
+        const listOfItems = this.props.items.map(item => <li key={item.id}>
+            <TodoItem toggleDone={this.toggleDone} {...item} />
+        </li>)
 
         return <div>
             <p>{this.props.items.length} items:</p>
             <ul>
-                {this.props.items.map(item => TodoItem(item))}
+                {listOfItems}
             </ul>
         </div>;
     }
